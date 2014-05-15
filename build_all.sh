f7db2346a1ca204053f732d3eb12ed30a5bd8ba5
Rodrigo Benenson <rodrigo.benenson@gmail.com>
2014-05-16 00:51:56 +0200

    build_all.sh script seems alive. Most compilations fail.

--- build_all.sh
+++ build_all.sh
@@ -1,22 +1,29 @@
-#!/usr/bin/sh
+#!/bin/sh
 # Do not use this script unless you know what you are doing,
 # start by reading readme.text to know which are the first step to do get this code base to compile.
 
 # change num threads depending on available RAM and Cpu cores.
 NUM_THREADS=15
 
+build_all()
+{
+
+for dirname in src/applications/*/ src/tests/*/ ;
+do
+  cd ${dirname}
+  cmake -D CMAKE_BUILD_TYPE=RelWithDebInfo .
+  make -j${NUM_THREADS}
+  cd -
+done
+
+}
+
 while true; do
-		read -p "Do you _really_ know what you are doing ? [y/N]" yn
+		read -p "Do you _really_ know what you are doing ? [y/N] " yn
 		case $yn in
-		[Yy]* ) fn_function;;
+		[Yy]* ) build_all; break;;
 		[Nn]* ) exit ;;
 		* ) echo "Please answer yes or no.";;
 		esac
 	done
-
-for dirname in ls -d src/applications/*/ src/tests/*/
-  cd ${dirname}
-  cmake -D CMAKE_BUILD_TYPE=RelWithDebInfo .
-  make -j${NUM_THREADS}
-done
 
