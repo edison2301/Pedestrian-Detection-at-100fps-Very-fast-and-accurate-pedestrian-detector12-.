115deb6f88c9aec392b43071271b1ca53753b447
Rodrigo Benenson <rodrigo.benenson@gmail.com>
2014-05-27 20:18:55 +0200

    Investigating the inria_pedestrians_cvpr2012.config.ini issue, seems related to gpu_very_fast when running on inria images

--- src/applications/objects_detection/ObjectsDetectionApplication.cpp
+++ src/applications/objects_detection/ObjectsDetectionApplication.cpp
@@ -85,7 +85,7 @@ typedef AbstractStixelWorldEstimator::ground_plane_corridor_t ground_plane_corri
 
 std::string ObjectsDetectionApplication::get_application_title()
 {
-    return "Stereo objects detection on a flat world. Rodrigo Benenson @ KULeuven. 2011-2012.";
+    return "Stereo objects detection on a flat world. Rodrigo Benenson, Markus Mathias, Mohamed Omran @ KULeuven & MPI-Inf. 2011-2014.";
 }
 
 
